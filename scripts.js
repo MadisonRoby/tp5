b26dd845fc88a773102719f2ec4e0f0f34855c93
Madison Roby <madison.roby@wsu.edu>
2021-03-12 04:47:08 +0000

    most recent update with the new DOM elements via JS

--- scripts.js
+++ scripts.js
@@ -1,25 +1,37 @@
 // load the event
 window.onload = function() {
-  
- //changes font size
-document.querySelector("#title").style.fontSize = "100px";}
- 
- //defines what's shown on click
-  function showIngredients() {
+
+  //changes font size
+  document.querySelector("#title").style.fontSize = "100px";
+}
+
+//defines what's shown on click
+function showIngredients() {
   document.getElementById("content1").style.visibility = "visible";
- } 
- function showPrep() {
+}
+
+function showPrep() {
   document.getElementById("content2").style.visibility = "visible";
 }
+
 function showEquipment() {
   document.getElementById("content3").style.visibility = "visible";
- }
+}
+
 function showDirections() {
   document.getElementById("content4").style.visibility = "visible";
 }
 
 //title color change via click  
 document.querySelector("#title");
-title.onclick = function () {
+title.onclick = function() {
   title.classList.toggle("clicked");
 }
+
+// new HTML elements
+x = document.getElementById("enjoy");
+x.innerHTML = "And that's how you make the world's best lasagna! Enjoy!";
+x.style.fontSize = '30px';
+x.style.fontFamily = 'Sigmar One';
+x.style.textAlign = 'center';
+x.style.color = 'saddlebrown';
